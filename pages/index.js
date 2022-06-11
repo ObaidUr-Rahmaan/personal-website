5ab7bfc44e057319152868b6b4c95d108b8f5d4b
ObaidUr-Rahmaan <Obaid.UrRahmaan@vonage.com>
2022-06-11 10:44:53 +0100

    Configure global styles

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <div className="text-5xl">Personal Website</div>;
+  return (
+    <div>
+      <Head>
+        <title>Obaid | Full-Stack Developer</title>
+        <meta
+          name="description"
+          content="Obaid is a full-stack developer who specializes in building web applications."
+        />
+        <link rel="icon" href="" />
+      </Head>
+      <h1 className="text-3xl">Personal Website</h1>
+    </div>
+  );
 }
