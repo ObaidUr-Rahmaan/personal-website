2a3629fce7d406fac7dbf9e018278686511542e7
ObaidUr-Rahmaan <Obaid.UrRahmaan@vonage.com>
2022-06-15 15:59:32 +0100

    Fix Skills component on-scroll behaviour

--- components/Skills.js
+++ components/Skills.js
@@ -4,7 +4,7 @@ import React from "react";
 const Skills = () => {
   return (
     <div id="skills" className="w-full p-2">
-      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full py-16">
         <p className="text-xl tracking-widest uppercase text-[#3a44a0]">
           Skills
         </p>
